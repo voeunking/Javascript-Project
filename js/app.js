edd1acec4c491befd440b24ea9fe10784e7b1a9e
KING.VOEUN <KING.VOEUN@pnc.local>
2025-12-24 19:54:45 +0700

    Fix CSS folder case issue

--- js/app.js
+++ js/app.js
@@ -1,10 +1,14 @@
-
-let notLogin = document.getElementById('notLoing');
-console.log(notLogin)
+let notLogin = document.querySelector('.link span a');
 let register = document.querySelector('.register-container')
+let login = document.getElementById('login');
+let re_form = document.getElementById('re-form');
 function display(){
-    let login = document.getElementById('.login');
     login.style.display = 'block';
+    register.style.display ='none';
 }
-
-register.addEventListener('click',display)
+notLogin.addEventListener('click',display)
+function show_regist(){
+     login.style.display = 'none';
+     register.style.display = 'block'
+}
+re_form.addEventListener('click',show_regist)
